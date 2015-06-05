c9e005d9fc5f39e1f263fe343e9b21aeab72e4d1
Islam <Islam@192.168.1.100>
2015-06-05 11:38:41 +0500

    My is Controller in CI

---
+++ application/controllers/blog.php
@@ -0,0 +1,19 @@
+<?php
+class Blog extends CI_Controller
+{
+    public function index()
+    {
+        echo"this is index function of blog controller";
+    }
+
+    public function add()
+    {
+        echo"Add to blog controller";
+    }
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
